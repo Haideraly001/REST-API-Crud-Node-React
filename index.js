5ae5f0de5f78f243a4a249f85bed9cded7a618f4
haideraly001 <haideralymughal@gmail.com>
2025-01-10 20:51:19 +0500

    async fs modal

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import readline from "readline"
-import fs, { write } from "fs"
+import fs from "fs"
 
 const text = fs.readFileSync("./controller/input.txt", "utf-8")
 
-console.log("text", text);
+// console.log("text", text);
 
 const content = `${text} you will become a billionaire`
 
@@ -12,3 +12,12 @@ const writetext = fs.writeFileSync("./controller/output.txt", content)
 
 
 
+fs.readFile("./controller/output.txt", "utf-8", (error1, data1) => {
+  console.log(data1);
+  fs.readFile("./controller/input.txt", "utf-8", (error2, data2) => {
+    console.log(data2);
+    fs.writeFile('/controller/append.txt', `${data1}\n\n${data2}\n\n${new Date()}`, (err, data3) => {
+      console.log(data3);
+    })
+  })
+})

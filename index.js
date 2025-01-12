1499f2c06e4b21e2284b5fb2ea21a57ac89a03c2
haideraly001 <haideralymughal@gmail.com>
2025-01-12 05:40:24 +0500

    req and resp

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ import readline from "readline"
 import fs from "fs"
 import http from "http"
 
+let a = 1;
+let b = 0
+let c = b + a
+const usedata = () => {
+  ++c
+  return c
+}
+
+const html = fs.readFileSync("./template/index.html", "utf-8")
+
 const server = http.createServer((req, res) => {
-  // res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello World")
-  console.log("a new request receive")
+  res.writeHead(200, { "Content-Type": "text/html" })
+  res.end(html)
+  console.log("New Request ", usedata())
 })
 
 server.listen("8000", "127.0.0.1", () => {
---
+++ template/app.js
@@ -0,0 +1,5 @@
+const h1 = document.querySelector("h1")
+
+const data = h1.addEventListener("click", () => {
+  alert("Hello Haider. I'm Edeet")
+})

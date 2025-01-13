7f3876ff573ba344a34862e8f8dfc2b1d122f6b7
haideraly001 <haideralymughal@gmail.com>
2025-01-13 17:14:43 +0500

    init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import readline from "readline"
-import fs, { readFileSync } from "fs"
+import fs from "fs"
 import http from "http"
 
 let a = 1;
@@ -24,42 +24,33 @@ const server = http.createServer((req, res) => {
       "Content-Type": "text/html",
       "my-header": "Home"
     })
-    res.end(html)
+    res.end(html.replace("{{%CONTENT%}}", "This is Home Page"))
+
   } else if (path.toLocaleLowerCase() === "/home") {
     res.writeHead(200, {
       "Content-Type": "text/html",
       "my-header": "Home"
     })
-    res.end("you are in Home Page")
+    res.end(html.replace("{{%CONTENT%}}", "This is Product Page"))
   } else if (path.toLocaleLowerCase() === "/about") {
     res.writeHead(200)
-    res.end("you are in About Page")
+    res.end(html.replace("{{%CONTENT%}}", "This is About Page"))
+
   } else if (path.toLocaleLowerCase() === "/data") {
     res.writeHead(200, {
       "Content-Type": "application/json",
       "my-header": "api"
     })
     res.end(jsonData)
-    // console.log(JSON.parse(jsonData));
-
-    const isUserJson = JSON.parse(jsonData)
-    const obj = {
-      name: isUserJson.users[0]?.firstName,
-      age: isUserJson.users[0]?.age,
-      image: isUserJson.users[0]?.image,
-    }
-
-    console.log(obj);
-
-
 
   } else {
     res.writeHead(400)
-    res.end("404 not found")
+    res.end(html.replace("{{%CONTENT%}}", "Error, 404 Page not found"))
+
   }
   console.log("New Request ", usedata())
 })
 
-server.listen("8000", "127.0.0.1", () => {
+server.listen("8000", () => {
   console.log("Server is running on port 127.0.0.1:8000")
 })

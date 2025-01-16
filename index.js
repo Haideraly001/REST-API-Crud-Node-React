68b8df443d35f15104748ac66a5f988dd4729eec
haideraly001 <haideralymughal@gmail.com>
2025-01-16 14:02:47 +0500

    get post and routes params in node.js

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
 import express from "express"
+import fs from "fs"
 
 const app = express()
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!")
+app.use(express.json())
+
+const movies = JSON.parse(fs.readFileSync("./user.json"))
+
+
+app.get("/app/v1/movies", (req, res) => {
+  res.status(200).json({
+    status: "200", data: {
+      "movies": movies
+    }
+  })
 })
 
 
-app
+app.post("/app/v1/movies", (req, res) => {
+  const newId = movies[movies.length - 1].id + 1
+  const users = req.body
+  const newObj = ({ id: newId, ...users })
+  console.log("newObj", newObj);
+  movies.push(newObj)
+  fs.writeFile("./user.json", JSON.stringify(movies), (err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
 
-const port = 3000;
+  res.status(200).json({
+    status: "200",
+    data: {
+      "movies": newObj
+    }
+  })
+})
+
+app.get("/app/v1/movies/:id/:name?", (req, res) => {
+  const params = req.params.id * 1
+  const uniqueMovie = movies.find((el) => el.id === params)
+  if (!uniqueMovie) {
+    res.status(404).json({
+      status: 404,
+      message: "Movie not found"
+    })
+  }
+
+  res.status(200).json({
+    data: uniqueMovie
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)

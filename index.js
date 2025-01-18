322144b9ac25f9ebe433c0a00816d174a52c89f0
haideraly001 <haideralymughal@gmail.com>
2025-01-18 22:49:48 +0500

    get  / post / route params method practice

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 import express from 'express'
 import fs from 'fs'
 
+const app = express()
+const port = 3000;
 
+app.use(express.json())
+
+const movies = JSON.parse(fs.readFileSync('./movies.json'))
+
+
+app.get('/movies', (req, res) => {
+  res.status(200).json({
+    movies: movies
+  })
+})
+
+app.post('/movies', (req, res) => {
+  const newMovies = req.body
+  const newId = movies[movies.length - 1].id + 1
+
+  const movieAdd = { id: newId, ...newMovies }
+  movies.push(movieAdd)
+
+  fs.writeFile("./movies.json", JSON.stringify(movies), (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+
+  res.status(200).json({
+    data: movies
+  })
+})
+
+app.get('/movies/:id', (req, res) => {
+  const id = req.params.id * 1
+  console.log(id);
+
+  const findMovie = movies.find((el) => el.id === id)
+  console.log('moives', findMovie);
+
+  if (!findMovie) {
+    res.status(404).json({
+      message: 'Movie not found'
+    })
+  }
+
+  res.status(200).json({
+    movie: findMovie
+  })
+
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})

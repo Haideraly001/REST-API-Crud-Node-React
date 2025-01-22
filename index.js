042c1faddfa19c91f4a3065d6d8d100f27850bba
haideraly001 <haideralymughal@gmail.com>
2025-01-22 17:58:14 +0500

    https method by myself crud on api

--- index.js
+++ index.js
@@ -1,86 +1,107 @@
-import express from "express"
-import fs from "fs"
+import express from 'express'
+import fs from 'fs'
 
 const app = express()
-
 const port = 3000
+
+const movies = JSON.parse(fs.readFileSync('./movies.json'))
 app.use(express.json())
 
-const movies = JSON.parse(fs.readFileSync("./movies.json"))
 
-app.get("/movies", (req, res) => {
-  res.status(200).json({ movies: movies })
-})
 
-app.post("/movies", (req, res) => {
+app.get('/movies', getRequest)
+
+app.post('/movies', postRequest)
+
+app.get("/movies/:id", getRequestById)
+
+app.patch("/movies/:id", patchRequestById)
+
+app.delete('/movies/:id', deleteRequestById)
+
+
+
+const getRequest = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: movies
+  })
+}
+
+const postRequest = (req, res) => {
   const newMovie = req.body
-  const id = movies[movies.length - 1].id + 1
-  const addMovie = { ...newMovie, id: id }
-  console.log(addMovie);
+  const Id = movies[movies.length - 1].id + 1
+  const modifyObj = { ...newMovie, id: Id }
 
-  movies.push(addMovie)
-  fs.writeFile("./movies.json", JSON.stringify(movies), (err) => {
-    if (err) console.log(err)
-  })
-  res.status(200).json({
-    status: 200,
-    newMovie
-  })
-})
-
-app.get("/movies/:id?", (req, res) => {
-  const Id = req.params.id * 1
-
-  const moviesById = movies.find((el) => el.id === Id)
-  if (!moviesById) {
-    res.status(400)
-  }
-  res.status(200).json({
-    movie: moviesById
-  })
-})
-
-app.patch("/movies/:id?", (req, res) => {
-  const Id = req.params.id * 1
-
-  const movieId = movies.find((el) => el.id === Id)
-  const editMovie = { ...movieId, ...req.body }
-
-  const movieIndex = movies.indexOf(movieId)
-  movies[movieIndex] = editMovie
+  movies.push(modifyObj)
 
   fs.writeFile('./movies.json', JSON.stringify(movies), (err) => {
     if (err) {
-      return res.status(400).json({
-        status: 400,
-        movies: "movies not found"
-      })
+      console.error(err)
     }
   })
+  res.status(201).json({
+    status: "success",
+    data: modifyObj
+  })
+}
+
+const getRequestById = (req, res) => {
+  const params = req.params.id * 1
+  const findMovie = movies.find((el) => el.id === params)
+  res.status(200).json({
+    status: "success",
+    data: findMovie
+  })
+}
+
+const patchRequestById = (req, res) => {
+  const params = req.params.id * 1
+
+  if (movies.length < params) {
+    res.status(404).json({
+      status: "error",
+      message: "Movie not found"
+    })
+  }
+
+  const findMovie = movies.find((el) => el.id === params)
+  // console.log(findMovie);
+  // console.log(req.body);
+
+  const EditMovie = { ...findMovie, ...req.body }
+  // const editMovie = Object.assign(findMovie, req.body)
+  // console.log(EditMovie);
+
+  const indexOf = movies.indexOf(findMovie)
+  movies[indexOf] = EditMovie
+
+  fs.writeFile('./movies.json', JSON.stringify(movies), (err) => {
+    if (err) {
+      return res.status(404).json(
+        err = "Error",
+        message = "Movie not Edit"
+      )
+    }
+  })
+  res.status(200).json({
+    status: "success",
+    data: EditMovie
+  })
+}
+
+const deleteRequestById = (req, res) => {
+  const params = req.params * 1
+  const findMovie = movies.find((el) => el.id === params)
+  const indexOf = movies.indexOf(findMovie)
+  movies.splice(indexOf, 1)
 
   res.status(200).json({
-    status: 200,
-    movie: editMovie
+    status: "success find is deleted",
+    data: {}
   })
-})
-
-
-app.delete("/movies/:id?", (req, res) => {
-  const Id = req.params.id * 1
-  const movieId = movies.find((el) => el.id === Id)
-  const movieIndex = movies.indexOf(movieId)
-  movies.splice(movieIndex, 1)
-  fs.writeFile("./movies.json", JSON.stringify(movies), (err) => {
-    res.status(204).json({
-      status: success,
-      movies: null
-    })
-  })
-
-
-})
-
+}
 
 app.listen(port, () => {
-  console.log(`server is listed at ${port}`);
+  console.log(`server is listen at ${port}`);
 })

1a681c3ebdd9895e8f58d23944dad82650e98ab8
haideraly001 <haideralymughal@gmail.com>
2025-01-31 16:00:04 +0500

    create an controller

---
+++ controller/formController.js
@@ -0,0 +1,106 @@
+import fs from "fs"
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+
+const form = JSON.parse(fs.readFileSync('./form.json',))
+
+const getForm = (req, res) => {
+  res.status(200).json({
+    status: "succss",
+    data: form
+  })
+}
+
+const postForm = (req, res) => {
+  const idx = form[form.length - 1].id * 1 + 1
+
+  const newForm = { ...req.body, id: idx }
+  form.push(newForm)
+
+  fs.writeFile('./form.json', JSON.stringify(form), (err) => {
+    if (!err) {
+      res.status(400).json({
+        status: "fail"
+      })
+    }
+  })
+  res.status(201).json({
+    status: "succss",
+    data: newForm
+  })
+
+}
+
+const getFormbyIdx = (req, res) => {
+  const param = req.params.id * 1
+  const findData = form.find((el) => el.id === param)
+
+  if (!findData) {
+    res.status(400).json({
+      status: "fail",
+      data: "there is no such id to show "
+    })
+  }
+  res.status(200).json({
+    status: "succss",
+    data: findData
+  })
+
+}
+
+const updateFormByIdx = (req, res) => {
+
+  const urlID = req.params.id * 1
+  const findData = form.find((el) => el.id === urlID)
+  const updateForm = { ...findData, ...req.body }
+
+  const idxUpdateFrom = form.indexOf(findData)
+  form[idxUpdateFrom] = updateForm
+
+  fs.writeFile("./form.json", JSON.stringify(form), (err) => {
+    if (!err) {
+      res.status(400).json({
+        status: "fail",
+        data: "file is failed to udpate"
+      })
+    }
+  })
+  res.status(200).json({
+    status: "succss",
+    updateDate: req.date,
+    data: updateForm
+  })
+}
+
+const deleteFormByIdx = (req, res) => {
+  const urlID = req.params.id * 1
+  const findData = form.find((el) => el.id === urlID)
+  const idxDelete = form.indexOf(findData)
+  form.splice(idxDelete, 1)
+
+  fs.writeFile("./form.json", JSON.stringify(form), (err) => {
+    if (!err) {
+      res.status(400).json({
+        status: "fail",
+      })
+    }
+  })
+  res.status(200).json({
+    status: "succss",
+    data: "obj delete"
+  })
+}
+
+// export default {
+//   getForm,
+//   postForm,
+//   updateFormByIdx,
+//   getFormbyIdx,
+//   deleteFormByIdx
+// }
+
+export { getForm, postForm, updateFormByIdx, getFormbyIdx, deleteFormByIdx };
--- router/form.js
+++ router/form.js
@@ -1,5 +1,7 @@
 import fs from "fs"
 import express from 'express'
+import { getForm, postForm, getFormbyIdx, updateFormByIdx, deleteFormByIdx } from "../controller/formController.js"
+// import fromController from "../controller/formController"
 
 const app = express()
 
@@ -10,92 +12,6 @@ const formRouter = express.Router()
 const form = JSON.parse(fs.readFileSync('./form.json',))
 
 
-const getForm = (req, res) => {
-  res.status(200).json({
-    status: "succss",
-    data: form
-  })
-}
-
-const postForm = (req, res) => {
-  const idx = form[form.length - 1].id * 1 + 1
-
-  const newForm = { ...req.body, id: idx }
-  form.push(newForm)
-
-  fs.writeFile('./form.json', JSON.stringify(form), (err) => {
-    if (!err) {
-      res.status(400).json({
-        status: "fail"
-      })
-    }
-  })
-  res.status(201).json({
-    status: "succss",
-    data: newForm
-  })
-
-}
-
-const getFormbyIdx = (req, res) => {
-  const param = req.params.id * 1
-  const findData = form.find((el) => el.id === param)
-
-  if (!findData) {
-    res.status(400).json({
-      status: "fail",
-      data: "there is no such id to show "
-    })
-  }
-  res.status(200).json({
-    status: "succss",
-    data: findData
-  })
-
-}
-
-const updateFormByIdx = (req, res) => {
-
-  const urlID = req.params.id * 1
-  const findData = form.find((el) => el.id === urlID)
-  const updateForm = { ...findData, ...req.body }
-
-  const idxUpdateFrom = form.indexOf(findData)
-  form[idxUpdateFrom] = updateForm
-
-  fs.writeFile("./form.json", JSON.stringify(form), (err) => {
-    if (!err) {
-      res.status(400).json({
-        status: "fail",
-        data: "file is failed to udpate"
-      })
-    }
-  })
-  res.status(200).json({
-    status: "succss",
-    updateDate: req.date,
-    data: updateForm
-  })
-}
-
-const deleteFormByIdx = (req, res) => {
-  const urlID = req.params.id * 1
-  const findData = form.find((el) => el.id === urlID)
-  const idxDelete = form.indexOf(findData)
-  form.splice(idxDelete, 1)
-
-  fs.writeFile("./form.json", JSON.stringify(form), (err) => {
-    if (!err) {
-      res.status(400).json({
-        status: "fail",
-      })
-    }
-  })
-  res.status(200).json({
-    status: "succss",
-    data: "obj delete"
-  })
-}
 
 app.use((req, res, next) => {
   req.date = new Date().toISOString()

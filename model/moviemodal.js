7b2235c3bd777709421d04d170fe57325af04cd0
haideraly001 <haideralymughal@gmail.com>
2025-02-04 21:31:06 +0500

    import export integration json using argv in db

--- model/moviemodal.js
+++ model/moviemodal.js
@@ -1,13 +1,58 @@
 import mongoose from "mongoose"
 
 const movieScheema = new mongoose.Schema({
+
   name: {
     type: String,
-    required: true,
+    required: [true, "Name feild is required"],
+    trim: true,
+    unique: true,
   },
-  category: {
+  description: {
     type: String,
-    required: true
+    required: [true, "description feild is required"],
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "Duration feild is required"],
+  },
+  rating: {
+    type: Number,
+  },
+  totalRating: {
+    type: Number,
+  },
+  releaseYear: {
+    type: Number,
+    required: [true, "Release Year feild is required"]
+  },
+  releaseDate: {
+    type: Date
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  genres: {
+    type: [String],
+    required: [true, "genres feild is required"]
+  },
+  directors: {
+    type: [String],
+    required: [true, "Director feild is required"]
+  },
+  coverImage: {
+    type: String,
+    required: [true, "Cover Image feild is required"]
+  },
+  actor: {
+    type: [String],
+    requried: [true, "actor feild is required"]
+  },
+  price: {
+    type: String,
+    requried: [true, "price feild is required"]
   }
 })
 

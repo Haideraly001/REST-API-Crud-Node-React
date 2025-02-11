919bb8d64dc355743ba8aba6651da450168ccbe8
haideraly001 <haideralymughal@gmail.com>
2025-02-12 02:48:39 +0500

    task for tomorrow

--- classes/class1.js
+++ classes/class1.js
@@ -1,26 +1,7 @@
-const person = {
-  calculAge() {
-    return new Date().getFullYear() - this.birthYear
-  },
-
-  initPerson(name, gender, birthYear) {
-    this.name = name;
-    this.gender = gender;
-    this.birthYear = birthYear;
-  }
-}
-
-const x = Object.create(person)
-
-// x.initPerson("Haider", "male", 1999)
-
-x.EmployeeInit = function (name, gender, birthYear, EmployeeID, EmployeeSalary) {
-  x.initPerson.call(this, name, gender, birthYear)
-  this.EmployeeID = EmployeeID;
-  this.EmployeeSalary = EmployeeSalary;
-}
-
-const y = Object.create(x)
-y.EmployeeInit("Haider", "male", 1999, 11845, 25000)
-console.log(y);
-
+// 1st create simple obj with function
+// 2nd create object with class
+// 3rd static method
+// 4th get and set obj data
+// 5th inheritance in function obj
+// 6th inheritance in class obj
+// 7th create an Object using Object.create method & inheritance in Object.create method

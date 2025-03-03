6ce68aebe3f00cc1861b37c882b3502351e22714
haideraly001 <haideralymughal@gmail.com>
2025-03-03 16:35:02 +0500

    auth

---
+++ model/usermodal.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import validator from "validator"
+
+const userSchemma = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Please Enter an Name"]
+  },
+  email: {
+    type: String,
+    require: [true, "please Enter an email"],
+    unique: true,
+    isLowercase: true,
+    validator: [validator.isEmail, "please Enter an valid email"]
+  },
+  photo: String,
+  password: {
+    type: String,
+    require: [true, "please Enter an password"],
+    minlength: 8,
+  },
+  confirmPassword: {
+    type: String,
+    require: [true, "please Enter an password"],
+    minlength: 8,
+  },
+})
+
+const User = mongoose.model("users", userSchemma)
+export default User;

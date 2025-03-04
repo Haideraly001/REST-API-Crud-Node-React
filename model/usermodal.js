d18f396586bd9d525e89261de97fc7e463b9d514
haideraly001 <haideralymughal@gmail.com>
2025-03-04 17:40:14 +0500

    bycript password with user

--- model/usermodal.js
+++ model/usermodal.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import validator from "validator"
+import bcrypt from "bcrypt"
 
 const userSchemma = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, "Please Enter an Name"]
+    required: [true, "Please Enter an Name"]
   },
   email: {
     type: String,
-    require: [true, "please Enter an email"],
+    required: [true, "please Enter an email"],
     unique: true,
     isLowercase: true,
     validator: [validator.isEmail, "please Enter an valid email"]
@@ -16,14 +17,30 @@ const userSchemma = new mongoose.Schema({
   photo: String,
   password: {
     type: String,
-    require: [true, "please Enter an password"],
+    required: [true, "please Enter an password"],
     minlength: 8,
   },
   confirmPassword: {
     type: String,
-    require: [true, "please Enter an password"],
+    required: [true, "please Enter an confirm password"],
     minlength: 8,
+    validate: {
+      validator: function (val) {
+        return val == this.password
+      },
+      message: "password & conform password does not match"
+    },
+
   },
+})
+
+
+userSchemma.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+
+  this.password = await bcrypt.hash(this.password, 10);
+  this.confirmPassword = undefined;
+  next()
 })
 
 const userModel = mongoose.model("users", userSchemma)

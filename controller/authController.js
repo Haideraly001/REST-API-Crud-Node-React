37070c6dfeece8db8d88b0c51ce06ea2e1d57865
haideraly001 <haideralymughal@gmail.com>
2025-03-09 23:21:11 +0500

    jwt auth

--- controller/authController.js
+++ controller/authController.js
@@ -1,12 +1,19 @@
 import userModel from '../model/usermodal.js'
+import jwt from 'jsonwebtoken'
+
 
 
 const authUser = async (req, res, next) => {
   try {
     const isUser = await userModel.create(req.body)
+
+    const token = jwt.sign({ id: isUser._id }, 'shh98-adlk4-nvxcn3', {
+      expiresIn: 1000000
+    });
     res.status(200).json({
       message: "success",
-      user: isUser
+      user: isUser,
+      token: token,
     })
   } catch (err) {
     res.status(401).json({
